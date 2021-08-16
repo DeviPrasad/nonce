ee384a858b1d294c580cdda7d6250641a17a1f57
DeviPrasad <dprasadm@gmail.com>
2021-08-17 01:04:26 +0530

    SQL Definitions Polished; improved.
    Tech notes - add a couple lines about oauth2 pkce.

--- nonce/sql/client.sql
+++ nonce/sql/client.sql
@@ -1,49 +1,114 @@
+-- mysql definitions
+USE DATABASE indus_oauth;
 
-DROP TABLE IF EXISTS indus_client;
+DROP TABLE IF EXISTS client_desc;
+DROP TABLE IF EXISTS client_secret;
+DROP TABLE IF EXISTS client_tokens;
 
-CREATE TABLE IF NOT EXISTS indus_client (
-    sln               INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY;
-    -- PGSQL sln      SERIAL;
-	client_id      	  varchar(255) NOT NULL,
-	client_secret  	  text NOT NULL,
-	redirect_uris  	  text NOT NULL,
-	grant_types  	  text NOT NULL,
-	response_types    text NOT NULL,
-	scope  			  text NOT NULL,
-	owner  			  text NOT NULL,
-	client_name  	  text NOT NULL,
-	policy_uri  	  text NOT NULL,
-	tos_uri  		  text NOT NULL,
-	client_uri  	  text NOT NULL,
-	logo_uri  		  text NOT NULL,
-	contacts  		  text NOT NULL,
-	confidential      boolean NOT NULL,
-    client_profile    enum (webapp, user-agent, native, device, pc)
+CREATE TABLE IF NOT EXISTS client_secret (
+    sln             BIGINT UNSIGNED UNIQUE,
+        -- protocol
+    client_id       BINARY(12) NOT NULL,
+    client_secret   BINARY(32) UNIQUE,
+        -- optional, 256-bit secrete exposed as a hex string
+        -- unique for multiple instances of a client using the same 'client_id'
+    expires_at      TIMESTAMP NOT NULL,
+        -- required, dynamic client registration. protocol.
+    issued_at       TIMESTAMP NOT NULL,
+        -- optional, dynamic client registration.
+    secret_type     TINYINT NOT NULL DEFAULT 0,
+        -- 1 = SYM, 2 = ASYM,
+    algorithm       VARCHAR(24) NOT NULL,
+    public_key      VARBINARY(512) DEFAULT NULL,
+    material        VARBINARY(32) DEFAULT NULL
+);
+FOREIGN KEY (client_id) REFERNCES client_desc(client_id);
+
+CREATE TABLE IF NOT EXISTS client_tokens (
+    client_id      	      BINARY(12) NOT NULL UNIQUE,
+    reg_access_token      TEXT,
+        -- OAuth 2.0 bearer token.
+        -- issued by AS thorugh the client registration endpoint.
+        -- used to authenticate the caller while accessing client configuration endpoint.
+    initial_access_token  TEXT
+);
+
+CREATE TABLE IF NOT EXISTS client_desc (
+    sln               BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    client_id      	  BINARY(12) NOT NULL,
+    client_name       TEXT(224) COLLATE utf8mb4_0900_bin,
+    client_type       TINYINT NOT NULL DEFAULT 1,
+        -- 'confidential' = 1, 'public' = 2,
+    application_type  TINYINT NOT NULL DEFAULT 1,
+        -- 'web' = 1, 'native' = 2
+    client_profile    TINYINT NOT NULL DEFAULT 1,
+        -- 'server-webapp' = 1, 'SPA' = 2, 'user-agent' = 6,
+        -- 'mobile-native-app' = 12, 'machine-local-app' = 18
+        -- 'pkce-aware' = 24
+    status            TINYINT NOT NULL DEFAULT 0,
+        -- BAD=0, PENDING=1, APPROVED=2, ACTIVE=4,
+        -- DEACTIVATED=8, LOCKED=16, BLOCKED=32, DELETED=64
+    registration_type  SMALLINT NOT NULL DEFAULT 0,
+        -- DYNAMIC = 1, FORM_POST = 2, ADMIN_CONSOLE = 4, CUSTOM = 8
+        -- VALIDATED = 1 << 8,
+        -- HAS_CLIENT_SECRET = 1 << 13
+    redirect_uris  	  TEXT NOT NULL,
+        -- required.
+    grant_types       SMALLINT NOT NULL DEFAULT 1,
+        -- required. ref: RFC 7591 - OAuth 2.0 Dynamic Registration.
+        -- 'authorization_code' = 1, 'password' = 2, 'client_credentials' = 4,
+        -- 'pkce' = 8, 'refresh_token' = 16, 'implicit' = 32
+        -- 'jwt-bearer' = 64, 'saml2-bearer' = 128
+    response_types    SMALLINT NOT NULL DEFAULT 1,
+        -- required.
+        -- 'code' = 1, 'token' = 2, 'id_token '= 4
+    logo_uri  		  TEXT,
+        -- optional.
+        -- when present, MUST point to a valid image file.
+        -- when present, server SHOULD display the image to the end-user during approval.
+    client_uri  	  TEXT,
+        -- optional. home page of the client.
+        -- when present, MUST point to a valid web page.
+        -- server SHOULD present this to the user so the user can safely follow the link.
+        -- RECOMMENDATION: use interstitial pages to reduce vulnerabilities.
+    policy_uri        TEXT,
+        -- optional. informs the end user how the profile data will be used.
+        -- when present, MUST point to a valid web page.
+        -- server SHOULD present this to the user.
+    tos_uri           TEXT,
+        -- optional. terms of service.
+        -- when present, MUST point to a valid web page.
+        -- server SHOULD present this to the user.
+    contacts          TEXT NOT NULL,
+        -- optional. array of email addresses of people responsible for the client.
+    scopes            TEXT,
+
+    needs_reg_config_access boolean DEFAULT FALSE,
     -- auth_method='none' when confidential=false
     token_endpoint_auth_method   VARCHAR(32) NOT NULL,
     -- UPDATE indus_client SET token_endpoint_auth_signing_alg = 'RS256' WHERE token_endpoint_auth_method = 'private_key_jwt';
     token_endpoint_auth_signing_alg VARCHAR(32) NOT NULL,
     client_secret_expires_at     INTEGER NOT NULL DEFAULT 0,
-    sector_identifier_uri  TEXT;,
-    jwks                 TEXT,
-    jwks_uri             TEXT,
+    sector_identifier_uri  TEXT,
+    jwks                 VARCHAR(2048),
+        -- optional. client's JSON Web Key Set document.
+    jwks_uri             VARCHAR(2048),
+        -- optional. URL for the client's JSON Web Key Set.
     request_uris         TEXT,
     audience             TEXT,
-    subject_type         VARCHAR(15) NOT NULL DEFAULT '',
+    subject_type         VARCHAR(15),
     allowed_cors_origins TEXT,
-    request_object_signing_alg   VARCHAR(10) NOT NULL,
-    userinfo_signed_response_alg VARCHAR(10) NOT NULL,
+    request_object_signing_alg    VARCHAR(10) NOT NULL,
+    userinfo_signed_response_alg  VARCHAR(10) NOT NULL,
+    frontchannel_logout_uri TEXT NULL,
+    frontchannel_logout_session_required BOOL NOT NULL DEFAULT FALSE,
+    post_logout_redirect_uris TEXT NULL,
+    backchannel_logout_uri TEXT NULL,
+    backchannel_logout_session_required BOOL NOT NULL DEFAULT FALSE,
 
-    frontchannel_logout_uri TEXT NULL;
-    frontchannel_logout_session_required BOOL NOT NULL DEFAULT FALSE;
-    post_logout_redirect_uris TEXT NULL;
-    backchannel_logout_uri TEXT NULL;
-    backchannel_logout_session_required BOOL NOT NULL DEFAULT FALSE;
-
-    additional_data TEXT NOT NULL DEFAULT "{}",
-
-    created_at timestamp NOT NULL DEFAULT now(),
-    updated_at timestamp NOT NULL DEFAULT now(),
-
-    CREATE UNIQUE INDEX nonce_client_index_id ON nonce_client (id);
+    misc         TEXT,
+    created_at   TIMESTAMP NOT NULL,
+    updated_at   TIMESTAMP
 );
+
+CREATE UNIQUE INDEX index_client_desc_client_id ON client_desc(client_id);
--- nonce/sql/oauth2.sql
+++ nonce/sql/oauth2.sql
@@ -1,90 +1,63 @@
+USE DATABASE indus_oauth;
+DROP TABLE IF EXISTS oauth2_access;
+DROP TABLE IF EXISTS oauth2_pkce;
+DROP TABLE IF EXISTS oauth2_refresh;
+DROP TABLE IF EXISTS oauth2_code;
 
-CREATE TABLE IF NOT EXISTS nonce_oauth2_access (
-	signature      	varchar(255) NOT NULL PRIMARY KEY,
-	request_id  	varchar(40) NOT NULL,
-	requested_at    timestamp NOT NULL DEFAULT now(),
-	client_id  		varchar(255) NOT NULL
-	scope  			text NOT NULL,
-	granted_scope 	text NOT NULL,
-	form_data  		text NOT NULL,
-	session_data  	text NOT NULL,
-    subject         varchar(255),
-    active          BOOL NOT NULL DEFAULT TRUE,
-    challenge_id    varchar(40) NULL,
-
-    CREATE UNIQUE INDEX nonce_oauth2_access_request_id_index ON nonce_oauth2_access (request_id),
-    CREATE INDEX nonce_oauth2_access_client_id_index ON nonce_oauth2_access (client_id),
-    CREATE INDEX nonce_oauth2_access_challenge_id_index ON nonce_oauth2_access (challenge_id)
+CREATE TABLE IF NOT EXISTS oauth2_access (
+    txid            BINARY(20) NOT NULL,
+    client_id  		BINARY(12) NOT NULL,
+    redirect_uri    TEXT, -- optional
+    scope  			TEXT, -- optional
+    state           VARBINARY(2048), -- recommended
+    granted_scope 	TEXT,
+    form_data  		TEXT,
+    session_data  	TEXT,
+    requested_at    TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE UNIQUE INDEX index_oauth2_access_txid ON oauth2_access (txid);
+CREATE INDEX index_oauth2_access_client_id ON oauth2_access (client_id);
 
-CREATE TABLE IF NOT EXISTS nonce_oauth2_refresh (
-	signature      	varchar(255) NOT NULL PRIMARY KEY,
-	request_id  	varchar(40) NOT NULL,
-	requested_at  	timestamp NOT NULL DEFAULT now(),
-	client_id  		varchar(255) NOT NULL
-	scope  			text NOT NULL,
-	granted_scope 	text NOT NULL,
-	form_data  		text NOT NULL,
-	session_data  	text NOT NULL,
-    subject         varchar(255),
-    active          BOOL NOT NULL DEFAULT TRUE,
-    challenge_id    varchar(40) NULL,
-    CREATE UNIQUE INDEX nonce_oauth2_refresh_request_id_index ON nonce_oauth2_refresh (request_id),
-    CREATE INDEX nonce_oauth2_refresh_client_id_index ON nonce_oauth2_refresh (client_id),
-    CREATE INDEX nonce_oauth2_refresh_challenge_id_index ON nonce_oauth2_refresh (challenge_id)
+CREATE TABLE IF NOT EXISTS oauth2_pkce (
+    txid            BINARY(20) NOT NULL,
+    client_id  		BINARY(12) NOT NULL,
+    redirect_uri    TEXT,
+    scope  			TEXT,
+    state           VARBINARY(2048),
+    code_challenge  VARBINARY(2048),
+    code_challenge_method VARCHAR(16),
+    granted_scope 	TEXT,
+    form_data  		TEXT,
+    session_data  	TEXT,
+    requested_at  	TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE UNIQUE INDEX index_oauth2_pkce_txid ON oauth2_pkce (txid);
+CREATE INDEX index_oauth2_pkce_client_id ON oauth2_pkce (client_id);
 
-CREATE TABLE IF NOT EXISTS nonce_oauth2_code (
-	signature      	varchar(255) NOT NULL PRIMARY KEY,
-	request_id  	varchar(40) NOT NULL,
-	requested_at  	timestamp NOT NULL DEFAULT now(),
-	client_id  		varchar(255) NOT NULL
-	scope  			text NOT NULL,
-	granted_scope 	text NOT NULL,
-	form_data  		text NOT NULL,
-	session_data  	text NOT NULL,
-    subject         varchar(255),
-    active          BOOL NOT NULL DEFAULT TRUE,
-    challenge_id    varchar(40) NULL,
-    CREATE INDEX nonce_oauth2_code_client_id_index ON nonce_oauth2_code (client_id),
-    CREATE INDEX nonce_oauth2_code_challenge_id_index ON nonce_oauth2_code (challenge_id)
+CREATE TABLE IF NOT EXISTS oauth2_refresh (
+    txid            BINARY(20) NOT NULL,
+    client_id  		BINARY(12) NOT NULL,
+    scope  			TEXT,
+    code_challenge  VARBINARY(2048),
+    code_challenge_method VARCHAR(16),
+    granted_scope 	TEXT,
+    form_data  		TEXT,
+    session_data  	TEXT,
+    requested_at  	TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE UNIQUE INDEX index_oauth2_refresh_txid ON oauth2_refresh (txid);
+CREATE INDEX index_oauth2_refresh_client_id ON oauth2_refresh (client_id);
 
-CREATE TABLE IF NOT EXISTS nonce_oauth2_oidc (
-	signature      	varchar(255) NOT NULL PRIMARY KEY,
-	request_id  	varchar(40) NOT NULL,
-	requested_at  	timestamp NOT NULL DEFAULT now(),
-	client_id  		varchar(255) NOT NULL
-	scope  			text NOT NULL,
-	granted_scope 	text NOT NULL,
-	form_data  		text NOT NULL,
-	session_data  	text NOT NULL,
-    subject         varchar(255),
-    active          BOOL NOT NULL DEFAULT TRUE,
-    challenge_id    varchar(40) NULL,
-    CREATE INDEX nonce_oauth2_oidc_client_id_index ON nonce_oauth2_oidc (client_id),
-    CREATE INDEX nonce_oauth2_oidc_challenge_id_index ON nonce_oauth2_oidc (challenge_id)
+CREATE TABLE IF NOT EXISTS oauth2_code (
+    txid            BINARY(20) NOT NULL,
+    client_id  		BINARY(12) NOT NULL,
+    scope  			TEXT,
+    granted_scope 	TEXT,
+    code_challenge  VARBINARY(2048),
+    code_challenge_method VARCHAR(16),
+    form_data  		TEXT,
+    session_data  	TEXT,
+    requested_at  	TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
-CREATE TABLE IF NOT EXISTS nonce_oauth2_pkce (
-	signature      	varchar(255) NOT NULL PRIMARY KEY,
-	request_id  	varchar(40) NOT NULL,
-	requested_at  	timestamp NOT NULL DEFAULT now(),
-	client_id  		varchar(255) NOT NULL
-	scope  			text NOT NULL,
-	granted_scope 	text NOT NULL,
-	form_data  		text NOT NULL,
-	session_data  	text NOT NULL,
-	subject 		varchar(255) NOT NULL,
-    active          BOOL NOT NULL DEFAULT TRUE,
-    challenge_id    varchar(40) NUL
-    CREATE INDEX nonce_oauth2_pkce_client_id_index ON nonce_oauth2_pkce (client_id),
-    CREATE INDEX nonce_oauth2_pkce_challenge_id_index ON nonce_oauth2_pkce (challenge_id)
-
-);
-
-REATE TABLE IF NOT EXISTS nonce_oauth2_jti_blacklist (
-	signature       varchar(64) NOT NULL PRIMARY KEY,
-	expires_at  	timestamp NOT NULL DEFAULT now(),
-    CREATE INDEX nonce_oauth2_jti_blacklist_expiry_index ON nonce_oauth2_jti_blacklist (expires_at)
-);
+CREATE INDEX index_oauth2_code_txid ON oauth2_code (txid);
+CREATE INDEX index_oauth2_code_client_id ON oauth2_code (client_id);
