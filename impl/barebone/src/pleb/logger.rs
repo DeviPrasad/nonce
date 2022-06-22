529f180abe282e86d53681e704fcc3202ce1b546
devi prasad <dprasadm@gmail.com>
2022-06-22 12:41:10 +0530

    Directory Cleanup.

--- impl/barebone/src/pleb/logger.rs
+++
@@ -1,8 +0,0 @@
-//use log::{debug, error, log_enabled, info, Level};
-//use env_logger::{Builder, Target};
-pub fn init_logging() -> bool {
-    env_logger::init();
-    //let mut builder = Builder::from_default_env();
-    //builder.target(Target::Stdout);
-    true
-}
